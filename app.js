1ca4bd7d4df0117197b081f26d15c910d325ddc7
Jesse Klein <jesse10klein@gmail.com>
2020-09-21 15:49:20 +1000

    refined files starting to add content

--- app.js
+++ app.js
@@ -11,23 +11,31 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname+'/views/index'));
+  res.render(path.join(__dirname, '/views/index'), {active: {home: true}});
 })
 
 app.get('/about', (req, res) => {
-  res.render(path.join(__dirname+'/views/about'));
+  res.render(path.join(__dirname, '/views/about'), {active: {about: true}});
+})
+
+app.get('/blog', (req, res) => {
+  res.render(path.join(__dirname, '/views/blog'), {active: {blog: true}});
 })
 
 app.get('/cart', (req, res) => {
-  res.render(path.join(__dirname+'/views/cart'));
+  res.render(path.join(__dirname, '/views/cart'), {active: {cart: true}});
+})
+
+app.get('/contact', (req, res) => {
+  res.render(path.join(__dirname, '/views/contact'), {active: {contact: true}});
 })
 
 app.get('/product', (req, res) => {
-  res.render(path.join(__dirname+'/views/product'));
+  res.render(path.join(__dirname, '/views/product'), {active: {product: true}});
 })
 
 app.get('/checkout', (req, res) => {
-  res.render(path.join(__dirname+'/views/checkout'));
+  res.render(path.join(__dirname+'/views/checkout'), {active: {checkout: true}});
 })
 
 
