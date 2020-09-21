04d4c0e8e1abd2b8410c130030fc9fda2d245d2f
Jesse Klein <jesse10klein@gmail.com>
2020-09-21 14:26:32 +1000

    converted to pug files

--- app.js
+++ app.js
@@ -4,13 +4,34 @@ const path = require("path");
 
 const app = express();
 
+
+app.set('view engine', 'pug')
+
 app.use(express.static('public'))
 
 
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/routes/index.html'));
+  res.render(path.join(__dirname+'/views/index'));
 })
+
+app.get('/about', (req, res) => {
+  res.render(path.join(__dirname+'/views/about'));
+})
+
+app.get('/cart', (req, res) => {
+  res.render(path.join(__dirname+'/views/cart'));
+})
+
+app.get('/product', (req, res) => {
+  res.render(path.join(__dirname+'/views/product'));
+})
+
+app.get('/checkout', (req, res) => {
+  res.render(path.join(__dirname+'/views/checkout'));
+})
+
+
+
 
 const port = 3000;
 app.listen(port, () => console.log(`App listening on port ${port}`));
